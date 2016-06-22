cc833571cbf833e9ac4af297a45a231e4329f5b0
Chris Brown <chris.brown@skookum.com>
2016-06-22 09:36:28 -0600

    class rewrite. now more prototype

--- src/vector.js
+++ src/vector.js
@@ -1,37 +1,35 @@
-// TODO add static functions to prototype, partially apply them on return obj?
 function Vector(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
+}
 
-  return {
-    x: x,
-    y, y,
-    add: function(v) {
-      var result = Object.assign({}, this);
-      result.x += v.x;
-      result.y += v.y;
-      return result;
-    },
-    minus: function(v) {
-      var result = Object.assign({}, this);
-      result.x -= v.x;
-      result.y -= v.y;
-      return result;
-    },
-    mag: function() {
-      return Math.sqrt(this.x * this.x + this.y * this.y);
-    },
-    norm: function(v) {
-      var result = Object.assign({}, this);
-      var mag = this.mag();
-      if (mag > 1) {
-        result.x /= mag;
-        result.y /= mag;
-      }
-      return result;
-    },
-    limit: function(max) {
-      // TODO implement vector.limit()
-    }
+Vector.prototype.add = function(v) {
+  var result = new Vector(this.x, this.y);
+  result.x += v.x;
+  result.y += v.y;
+  return result;
+}
+
+Vector.prototype.minus = function(v) {
+  var result = new Vector(this.x, this.y);
+  result.x -= v.x;
+  result.y -= v.y;
+  return result;
+}
+
+Vector.prototype.mag = function() {
+  return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+
+Vector.prototype.norm = function() {
+  var result = new Vector(this.x, this.y);
+  var mag = this.mag();
+  if (mag > 1) {
+    result.x /= mag;
+    result.y /= mag;
   }
+  return result;
 }
 
 export default Vector;
+
